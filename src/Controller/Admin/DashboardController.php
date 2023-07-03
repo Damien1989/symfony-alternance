b4fb8d6adeb2a674971180d7d4bf1c6e237dadb4
damien <contact.damiendiaz@gmail.com>
2023-07-03 20:56:48 +0200

    easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    private $adminUrlGenerator;
+
+    public function __construct(AdminUrlGenerator $adminUrlGenerator) {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
+    #[Route('/admin', name: 'admin')]
+
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator->setController(UserCrudController::class)
+            ->generateUrl();
+
+        return $this->redirect($url);
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Dam\'s');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+    }
+}
