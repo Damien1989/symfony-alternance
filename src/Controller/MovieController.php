b6828e3f633edbdc661ffd0c1e03b0561273bfe8
damien <contact.damiendiaz@gmail.com>
2023-09-28 21:34:19 +0200

    font awesome

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    #[Route('/movie', name: 'app_movie')]
+    public function index(): Response
+    {
+
+
+
+        return $this->render('movie/index.html.twig', [
+
+        ]);
+    }
+}
