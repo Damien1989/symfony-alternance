c05dd223818da67eabc7d101e35b00d6421e5b1f
damien <contact.damiendiaz@gmail.com>
2024-01-01 20:48:11 +0100

    order style with button

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,15 +2,20 @@
 
 namespace App\Classe;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart {
 
     private $session;
+    private $Requeststack;
 
     public function __construct(RequestStack $stack)
     {
         $this->session = $stack->getSession();
+        $this->requestStack = $stack;
     }
 
     public function add($id)
@@ -26,20 +31,6 @@ class Cart {
 
         $this->session->set('cart', $cart);
     }
-
-    public function decrease($id)
-    {
-        $cart = $this->session->get('cart', []);
-        if($cart[$id] > 1) {
-            $cart[$id]--;
-        } else {
-            unset($cart[$id]);
-        }
-        return $this->session->set('cart', $cart);
-    }
-
-
-
 
     public function get()
     {
@@ -58,5 +49,29 @@ class Cart {
         unset ($cart[$id]);
 
         return $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+        if($cart[$id] > 1) {
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+        return $this->session->set('cart', $cart);
+    }
+
+    public function getFull(ProductRepository $productRepository)
+    {
+        $cartComplete = [];
+
+        foreach ($this->requestStack->getSession()->get('cart') as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $productRepository->findOneBy(['id' => $id]),
+                'quantity' => $quantity
+            ];
+        }
+        return $cartComplete;
     }
 }
--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     #[Route('/commande', name: 'order')]
-    public function index(): Response
+    public function index(Cart $cart, ProductRepository $productRepository, Request $request): Response
     {
         if (!$this->getUser()->getAddresses()->getValues())
         {
@@ -20,8 +23,15 @@ class OrderController extends AbstractController
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
         return $this->render('order/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'cart' => $cart->getFull($productRepository)
         ]);
     }
 }
