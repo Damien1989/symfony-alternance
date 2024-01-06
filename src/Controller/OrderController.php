6366e80b6975d0789f34cece75d32add8d5596b3
damien <contact.damiendiaz@gmail.com>
2024-01-06 21:14:34 +0100

    purchase funnel stripe

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -67,6 +67,8 @@ class OrderController extends AbstractController
             $delivery_content .= '<br/>' . $delivery->getCountry();
 
             $order = new Order();
+            $reference = $date->format('dmY').'-'.uniqid();
+            $order->setReference($reference);
             $order->setUser($this->getUser());
             $order->setCreatedAt($date);
             $order->setCarrierName($carriers->getName());
@@ -86,10 +88,12 @@ class OrderController extends AbstractController
                 $this->entityManager->persist($orderDetails);
             }
              $this->entityManager->flush();
+
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull($productRepository),
                 'carrier' => $carriers,
-                'delivery' => $delivery_content
+                'delivery' => $delivery_content,
+                'reference' => $order->getReference()
             ]);
         }
         return $this->redirectToRoute('cart');
